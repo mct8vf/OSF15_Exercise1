8310391eb9a39aa01b2b867dbf117e4024e5d305
mct8vf <mct8vf@mail.missouri.edu>
2015-09-11 10:44:12 -0500

    Update command.c

--- Exercise1/command.c
+++ Exercise1/command.c
@@ -8,12 +8,18 @@
 #define MAX_CMD_COUNT 50
 #define MAX_CMD_LEN 25
 
-
+	//TODO 4
 	//TODO FUNCTION COMMENT
+	/*
+ * PURPOSE: To parse or split up some input(commands) and store the individual commands into an array
+ * INPUTS:	char* input which is to be parsed, Commands_t** cmd where the commands are to be stored
+ * RETURN:	'true' of operation compeleted without issue, false if otherwise
+ **/
 bool parse_user_input (const char* input, Commands_t** cmd) {
 	
 	//TODO ERROR CHECK INCOMING PARAMETERS
-
+	if((!input)||(!cmd))
+		return false;
 	char *string = strdup(input);
 	
 	*cmd = calloc (1,sizeof(Commands_t));
@@ -37,15 +43,26 @@ bool parse_user_input (const char* input, Commands_t** cmd) {
 }
 
 	//TODO FUNCTION COMMENT
+	/*
+ * PURPOSE: Free the list of commands
+ * INPUTS:	Commands_t** cmd containing the commands to be deleted
+ * RETURN:	None
+ **/
 void destroy_commands(Commands_t** cmd) {
 
 	//TODO ERROR CHECK INCOMING PARAMETERS
-	
-	for (int i = 0; i < (*cmd)->num_cmds; ++i) {
-		free((*cmd)->cmds[i]);
+	if(!cmd)
+	{
+		printf("Error: NULL\n");
 	}
-	free((*cmd)->cmds);
-	free((*cmd));
-	*cmd = NULL;
+	else{
+		
+		for (int i = 0; i < (*cmd)->num_cmds; ++i) {
+			free((*cmd)->cmds[i]);
+		}
+		free((*cmd)->cmds);
+		free((*cmd));
+		*cmd = NULL;
+	}
 }
 
